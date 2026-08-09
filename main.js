eca292b2ac346a7c16e290fc0a45dcec327453e9
yousefzinsazk78 <yousefzinsazk78@gmail.com>
2026-08-09 12:46:57 +0330

    add console.log() and add some css in style.css

--- main.js
+++ main.js
@@ -1,11 +1,1 @@
-let body = document.querySelector("h1");
-const html = document.querySelector("html")
-
-body.style.backgroundColor = "lightblue";
-
-body.addEventListener('click' , () => {
-    console.log("this is tesg")
-})
-html.addEventListener('click', () => {
-    document.querySelector('body').style.backgroundColor = "yellow";
-})
+console.log("hello world")
